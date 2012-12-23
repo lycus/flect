5ece062f733fb71a9ca7e5bd5c6e5e17219a1454
alexrp <alex@lycus.org>
2012-12-23 21:54:25 +0100

    Move configuration validation into the configuration script.

--- config.exs
+++ config.exs
@@ -61,6 +61,47 @@ end
 arch = get.("FLECT_ARCH", arch, false)
 os = get.("FLECT_OS", os, false)
 abi = get.("FLECT_ABI", abi, false)
+
+unless os in ["none", "aix", "android", "dragonflybsd", "freebsd", "hurd", "haiku", "ios", "linux", "darwin", "openbsd", "solaris", "windows"] do
+    IO.puts("Error: Invalid operating system #{os} (FLECT_OS)")
+    System.halt(1)
+end
+
+arch_valid = case os do
+    "none" -> arch in ["arm", "ia64", "mips", "hppa", "ppc", "x86"]
+    "aix" -> arch in ["ppc"]
+    "android" -> arch in ["arm", "mips", "x86"]
+    "dragonflybsd" -> arch in ["x86"]
+    "freebsd" -> arch in ["arm", "ia64", "mips", "ppc", "x86"]
+    "hurd" -> arch in ["x86"]
+    "haiku" -> arch in ["x86"]
+    "ios" -> arch in ["arm"]
+    "linux" -> arch in ["arm", "ia64", "mips", "hppa", "ppc", "x86"]
+    "darwin" -> arch in ["ppc", "x86"]
+    "openbsd" -> arch in ["arm", "mips", "hppa", "ppc", "x86"]
+    "solaris" -> arch in ["ppc", "x86"]
+    "windows" -> arch in ["ia64", "x86"]
+end
+
+unless arch_valid do
+    IO.puts("Error: Invalid architecture #{arch} for operating system #{os} (FLECT_ARCH)")
+    System.halt(1)
+end
+
+abi_valid = case arch do
+    "arm" -> abi in ["arm-thumb", "arm-soft", "arm-softfp", "arm-hardfp"]
+    "ia64" -> abi in ["ia64-psabi"]
+    "mips" -> abi in ["mips-o32", "mips-n32", "mips-o64", "mips-n64"]
+    "hppa" -> abi in ["hppa-pa32", "hppa-pa64"]
+    "ppc" -> abi in ["ppc-softfp", "ppc-hardfp", "ppc-ppc64"]
+    "x86" -> abi in ["x86-ms32", "x86-sysv32", "x86-ms64", "x86-sysv64", "x86-x32"]
+end
+
+unless abi_valid do
+    IO.puts("Error: Invalid ABI #{abi} for architecture #{arch} (FLECT_ABI)")
+    System.halt(1)
+end
+
 cross = get_bool.("FLECT_CROSS", "false")
 
 IO.puts("")
@@ -69,9 +110,21 @@ IO.puts("")
 
 cc = get.("FLECT_CC", "clang", false)
 cc_type = get.("FLECT_CC_TYPE", "gcc", false)
+
+unless cc_type in ["gcc", "msvc"] do
+    IO.puts("Error: Invalid C99 compiler type #{cc_type} (FLECT_CC_TYPE)")
+    System.halt(1)
+end
+
 cc_args = get.("FLECT_CC_ARGS", "", true)
 ld = get.("FLECT_LD", "ld", false)
 ld_type = get.("FLECT_LD_TYPE", "ld", false)
+
+unless ld_type in ["ld", "msvc"] do
+    IO.puts("Error: Invalid linker type #{ld_type} (FLECT_LD_TYPE)")
+    System.halt(1)
+end
+
 ld_args = get.("FLECT_LD_ARGS", "", true)
 
 IO.puts("")
--- lib/target_error.ex
+++
@@ -1,8 +0,0 @@
-defexception Flect.TargetError, error: "" do
-    record_type(error: String.t())
-
-    @spec message(Flect.TargetError.t()) :: String.t()
-    def message(self) do
-        self.error()
-    end
-end
