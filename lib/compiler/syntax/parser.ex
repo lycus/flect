283c8b2042a9d8001b47650a822a953f9c7bcc4f
alexrp <alex@lycus.org>
2012-12-24 13:11:30 +0100

    Add expect_token() function, and make next_token() drop comments (for now).

--- lib/compiler/syntax/parser.ex
+++ lib/compiler/syntax/parser.ex
@@ -10,14 +10,32 @@ defmodule Flect.Compiler.Syntax.Parser do
     end
 
     @spec next_token([Flect.Compiler.Syntax.Token.t()], Flect.Compiler.Syntax.Location.t(),
-                     boolean()) :: {Flect.Compiler.Syntax.Token.t(), [Flect.Compiler.Syntax.Token.t()]} | :eof
+                     boolean()) :: {atom(), Flect.Compiler.Syntax.Token.t(),
+                                    [Flect.Compiler.Syntax.Token.t()], Flect.Compiler.Syntax.Location.t()} | :eof
     defp next_token(tokens, loc, eof // false) do
         case tokens do
-            [h | t] -> {h, t}
+            [h | t] ->
+                case h.type() do
+                    # TODO: Attach comments to AST nodes.
+                    a when a in [:line_comment, :block_comment] -> next_token(t, h.location(), eof)
+                    a -> {a, h, t, h.location()}
+                end
             [] when eof -> :eof
             _ -> raise(Flect.Compiler.Syntax.SyntaxError, [error: "Unexpected end of file",
-                                                           file: loc.file(),
                                                            location: loc])
         end
+    end
+
+    @spec expect_token([Flect.Compiler.Syntax.Token.t()], Flect.Compiler.Syntax.Location.t(), atom(),
+                        String.t()) :: {Flect.Compiler.Syntax.Token.t(), [Flect.Compiler.Syntax.Token.t()]} | :eof
+    defp expect_token(tokens, loc, type, str) do
+        tup = {t, tok, _, l} = next_token(tokens, loc)
+
+        if t != type do
+            raise(Flect.Compiler.Syntax.SyntaxError, [error: "Expected #{str}, but got #{tok.value()}",
+                                                      location: l])
+        end
+
+        tup
     end
 end
