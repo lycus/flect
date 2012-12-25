ec25169bdbc20cb211b17ef015becbd032d14305
alexrp <alex@lycus.org>
2012-12-25 18:51:27 +0100

    Add a named_children() function to Node.

--- lib/compiler/syntax/node.ex
+++ lib/compiler/syntax/node.ex
@@ -1,9 +1,11 @@
 defrecord Flect.Compiler.Syntax.Node, type: nil,
                                       location: nil,
                                       tokens: [],
+                                      named_children: [],
                                       children: [] do
     record_type(type: atom(),
                 location: Flect.Compiler.Syntax.Location.t(),
                 tokens: [{atom(), Flect.Compiler.Syntax.Token.t()}, ...],
+                named_children: [{atom(), t()}],
                 children: [t()])
 end
