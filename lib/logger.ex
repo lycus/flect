8f3e2c689aae1e462b23f124e47fb71159c02b09
alexrp <alex@lycus.org>
2013-03-10 09:30:20 +0100

    Add a FLECT_COLORS environment variable to disable colors.

--- lib/logger.ex
+++ lib/logger.ex
@@ -7,13 +7,14 @@ defmodule Flect.Logger do
     (where `msg` is a binary) to that PID instead of being printed to standard
     output.
 
-    Note also that if `:flect_event_pid` is set or the current terminal is
-    not ANSI-compatible, colored output will be disabled.
+    Note also that if `:flect_event_pid` is set, the current terminal is
+    not ANSI-compatible, or the `FLECT_COLORS` environment variable is set to
+    `0`, colored output will be disabled.
     """
 
     @spec colorize(String.t(), String.t()) :: String.t()
     defp colorize(str, color) do
-        emit = IO.ANSI.terminal?() && :application.get_env(:flect, :flect_event_pid) == :undefined
+        emit = IO.ANSI.terminal?() && :application.get_env(:flect, :flect_event_pid) == :undefined && System.get_env("FLECT_COLORS") != "0"
         IO.ANSI.escape_fragment("%{#{color}, bright}#{str}:%{reset} ", emit)
     end
 
