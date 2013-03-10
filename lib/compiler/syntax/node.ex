23d355cc55f846a6bcfcea7c73ee074eef931e73
alexrp <alex@lycus.org>
2013-03-10 06:22:04 +0100

    New, significantly more readable AST dump format.

--- lib/compiler/syntax/node.ex
+++ lib/compiler/syntax/node.ex
@@ -21,7 +21,7 @@ defrecord Flect.Compiler.Syntax.Node, type: nil,
                 location: Flect.Compiler.Syntax.Location.t(),
                 tokens: [{atom(), Flect.Compiler.Syntax.Token.t()}, ...],
                 named_children: [{atom(), t()}],
-                children: [t()],
+                children: [{atom(), t()}],
                 data: term())
 
     @doc """
@@ -37,33 +37,14 @@ defrecord Flect.Compiler.Syntax.Node, type: nil,
 
     @spec do_format(t(), String.t()) :: String.t()
     defp do_format(node, indent) do
-        loc = fn(loc) ->
-            l = integer_to_binary(loc.line())
-            c = integer_to_binary(loc.column())
+        loc = fn(loc) -> "(#{loc.line()},#{loc.column()})" end
 
-            "<\"" <> loc.file() <> "\", " <> l <> ", " <> c <> ">"
-        end
+        str = indent <> "#{atom_to_binary(node.type())} #{loc.(node.location())} "
+        str = str <> "[ " <> Enum.join((lc {_, t} inlist node.tokens(), do: "\"#{t.value()}\" #{loc.(t.location())}"), ", ") <> " ]\n"
+        str = str <> indent <> "{\n"
+        str = str <> Enum.join(lc {_, child} inlist node.children(), do: do_format(child, indent <> "    ") <> "\n")
+        str = str <> indent <> "}"
 
-        join = ",\n  " <> indent
-
-        tokens = Enum.map(node.tokens(), fn({key, tok}) ->
-            atom_to_binary(key) <> ": \"" <> tok.value() <> "\" " <> loc.(tok.location())
-        end)
-
-        children = Enum.map(node.children(), fn(child) ->
-            do_format(child, indent <> "  ")
-        end)
-
-        named_children = Enum.map(node.named_children(), fn({key, child}) ->
-            atom_to_binary(key) <> ": " <> do_format(child, indent <> " ")
-        end)
-
-        acc = "(" <> atom_to_binary(node.type()) <> " " <> loc.(node.location()) <> ",\n"
-        acc = acc <> indent <> " (" <> Enum.join(tokens, join) <> "),\n"
-        acc = acc <> indent <> " (" <> Enum.join(children, join) <> "),\n"
-        acc = acc <> indent <> " (" <> Enum.join(named_children, join) <> ")\n"
-        acc = acc <> indent <> ")"
-
-        acc
+        str
     end
 end
