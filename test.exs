86d6fac3834289b1ce04e408e9a2cec8cc6e8212
alexrp <alex@lycus.org>
2013-05-13 07:17:56 +0200

    Specific tests can now be skipped in a particular pass depending on an arbitrary condition.
    Also some general test runner cleanup.

--- test.exs
+++ test.exs
@@ -13,6 +13,17 @@ files = :file.list_dir(path) |>
         Enum.map(fn(x) -> :unicode.characters_to_binary(x) end) |>
         Enum.sort()
 
+vars = lc {a, s} inlist [arch: "ARCH",
+                         os: "OS",
+                         abi: "ABI",
+                         fpabi: "FPABI",
+                         endian: "ENDIAN",
+                         cross: "CROSS",
+                         cc_type: "CC_TYPE",
+                         ld_type: "LD_TYPE"] do
+    {a, System.get_env("FLECT_" <> s) |> String.replace("-", "_") |> binary_to_atom()}
+end
+
 otp = :erlang.system_info(:otp_release)
 
 if otp >= 'R16B' && System.get_env("FLECT_COVER") == "1" do
@@ -52,21 +63,26 @@ results = Enum.map(passes, fn(pass) ->
             end
         end
 
-        extra_args = case :file.consult(file <> "." <> pass[:pass] <> ".arg") do
-            {:ok, [list]} -> Enum.map(list, fn(arg) -> :unicode.characters_to_binary(arg) end)
-            {:error, :enoent} -> []
+        {extra_args, expr, condition} = case :file.consult(file <> "." <> pass[:pass] <> ".arg") do
+            {:ok, [args, expr]} ->
+                args = Enum.map(args, fn(arg) -> :unicode.characters_to_binary(arg) end)
+                expr = :unicode.characters_to_binary(expr)
+                condition = elem(Code.eval(expr, vars), 0)
+
+                {args, expr, condition}
+            {:error, :enoent} -> {[], "", true}
         end
 
-        if is_list(pass[:command]) do
-            args = Enum.map(pass[:command], fn(arg) ->
-                arg |>
-                :unicode.characters_to_binary() |>
-                String.replace("<file>", file) |>
-                String.replace("<name>", Path.rootname(file))
-            end) ++ extra_args
+        args = Enum.map(pass[:command], fn(arg) ->
+            arg |>
+            :unicode.characters_to_binary() |>
+            String.replace("<file>", file) |>
+            String.replace("<name>", Path.rootname(file))
+        end) ++ extra_args
 
-            IO.write("    flect #{args |> Enum.join(" ")} ... ")
+        IO.write("    flect #{args |> Enum.join(" ")} ... ")
 
+        if condition do
             {opts, rest} = Flect.Application.parse(args)
 
             cfg = Flect.Config[tool: binary_to_atom(hd(rest)),
@@ -92,32 +108,8 @@ results = Enum.map(passes, fn(pass) ->
 
             check.(file, pass, text, code)
         else
-            cmd = pass[:command] |>
-                  :unicode.characters_to_binary() |>
-                  String.replace("<file>", file) |>
-                  String.replace("<name>", Path.rootname(file))
-
-            if extra_args != [] do
-                cmd = cmd <> " " <> Enum.join(extra_args, " ")
-            end
-
-            IO.write("    #{cmd} ... ")
-
-            port = Port.open({:spawn, :unicode.characters_to_list(cmd)}, [:stream,
-                                                                          :binary,
-                                                                          :exit_status,
-                                                                          :hide])
-
-            recv = fn(recv, port, acc) ->
-                receive do
-                    {^port, {:data, data}} -> recv.(recv, port, acc <> data)
-                    {^port, {:exit_status, code}} -> {acc, code}
-                end
-            end
-
-            {text, code} = recv.(recv, port, "")
-
-            check.(file, pass, text, code)
+            IO.puts(IO.ANSI.escape("%{blue, bright}skip (#{expr})"))
+            nil
         end
     end)
 end)
@@ -127,11 +119,13 @@ File.cd!(Path.join("..", ".."))
 results = List.flatten(results)
 
 test_passes = Enum.count(results, fn(x) -> x end)
-test_failures = Enum.count(results, fn(x) -> !x end)
+test_skips = Enum.count(results, fn(x) -> x == nil end)
+test_failures = Enum.count(results, fn(x) -> x == false end)
 tests = test_passes + test_failures
 
 IO.puts("")
 IO.puts(IO.ANSI.escape_fragment("  %{yellow, bright}#{tests}%{reset} test passes executed, " <>
+                                "%{blue, bright}#{test_skips}%{reset} skipped, " <>
                                 "%{green, bright}#{test_passes}%{reset} successful, " <>
                                 "%{red, bright}#{test_failures}%{reset} failed"))
 IO.puts("")
